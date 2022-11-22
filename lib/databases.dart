9d8f3ea091cdc972334362a93e52847a723edc6f
powerlego <curl.nicholas@gmail.com>
2022-11-22 13:09:53 -0500

    added local implmentations for doc generation

--- lib/databases.dart
+++ lib/databases.dart
@@ -1,4 +1,6 @@
 library mtg_databases;
 
 export 'src/database/database.dart';
+export 'src/database/cards/cards_database_local.dart';
+export 'src/database/currency/currency_database_local.dart';
 export 'src/database/entries.dart';
